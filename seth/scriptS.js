e2c693bb1f25fe042a12c797cae5215d8e5090b8
SethJohnson25750 <johnsonseth209@gmail.com>
2025-06-27 03:07:03 -0700

    done

--- seth/scriptS.js
+++ seth/scriptS.js
@@ -3,20 +3,36 @@ const rock = document.getElementById('rock')
 const score = document.getElementById('score')
 
 function jump() {
+   if (!cat.classList.contains('jump-animation')) {
     cat.classList.add('jump-animation');
     setTimeout(() => {
-    cat.classList.remove('jump-animation');
-    }, 250);
+      cat.classList.remove('jump-animation');
+    }, 500);
+  }
 }
 
-document.addEventListener("keypress", function() {
-  if (cat.classList.contains('jump-animation')) {
-    jump(); 
-  }  
-});
+document.addEventListener("keydown", jump);
+  
 
 setInterval(() => {
+score.innerText++;  
 const catTop = parseInt(window.getComputedStyle(cat)
-    .getPropertyValue('top'))
-    console.log(catTop)
-}, 50); 
+    .getPropertyValue('top'));
+const rockLeft = parseInt(window.getComputedStyle(rock)
+    .getPropertyValue('left')); 
+    
+   if(rockLeft < 0) {
+    rock.style.display = 'none'; 
+   } else {
+    rock.style.display = '';
+   }
+
+   if (rockLeft < 50 && rockLeft > 0 && catTop > 150 ) {
+    alert("Your Score:" + score.innerText + 
+    " Want to try again?");
+    location.reload();
+   }
+
+
+  
+  }, 50);  
